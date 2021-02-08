c35fcf1f8f425332af277a4e52ebc2ca0cffdb95
Rafael Torres Escobar <torresc.rafael@gmail.com>
2021-02-08 00:52:27 -0600

    python scripts 01, 02, and 03 in slides simplex

---
+++ 03_simplex/scripts/example03-01.py
@@ -0,0 +1,68 @@
+"""
+Max Z = 3x1 + 4x2 
+x1 + x2 <= 450
+2x1 + x2 <= 600
+
+x1, x2 >= 0
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(-10,500, 50)
+
+eq1 = 450 - x    # x2 = 450 - x1
+eq2 = 600 - 2*x  # x2 = 600 - 2x1
+zeq = lambda z,x:(z - 3*x)/4  # x2 = (Z - 3x1)/4
+
+# ======================================================================
+# Plot 
+plt.figure(figsize=(5,5))
+plt.axis("equal")
+
+plt.axvline(0, color='gray')
+plt.axhline(0, color='gray')
+
+plt.plot(x,eq1, lw=3, label=r"$x_1 + x_2 \leq 450$")
+plt.plot(x,eq2, lw=3, label=r"$2x_1 + x_2 \leq 600$")
+
+plt.plot(x, zeq(0, x), color='magenta', ls="--", lw=2, alpha=0.4, label=r"$\max\, Z = 3x_1 + 4x_2$")  # line plot for the best z function
+for i in np.linspace(500, 1500, 4):  # z function lines
+    plt.plot(x, zeq(i,x), color='magenta', lw=2, ls="--", alpha=0.4)
+        
+plt.legend(fontsize=12, loc="upper right")
+plt.xlim(-10,500)
+plt.ylim(-10,500)
+plt.show()
+# ======================================================================
+points = [(0,0), (0,450), (150, 300), (300,0)]  # intersecting points
+zvalues = [3*x1 + 4*x2 for x1,x2 in points]
+best_index, best_value = max(enumerate(zvalues), key=lambda t:t[1])
+best_point = points[best_index]
+coordinates = list(zip(*points))
+
+# ======================================================================
+# Plot the feasible region
+plt.figure(figsize=(5,5))
+plt.axis("equal")
+
+plt.axvline(0, color='gray')
+plt.axhline(0, color='gray')
+
+plt.plot(x,eq1, lw=3, label=r"$x_1 + x_2 \leq 450$")
+plt.plot(x,eq2, lw=3, label=r"$2x_1 + x_2 \leq 600$")
+
+for i in np.linspace(500,1500,4):  # z function lines
+    plt.plot(x, zeq(i,x), color='magenta', lw=2, ls="--", alpha=0.4)
+    
+for point in points:  # corner points
+    plt.plot(*point, marker="o", color="k", ms=7)
+    
+plt.fill(*coordinates, facecolor="yellow", edgecolor="gray", alpha=0.4) # feasible region
+
+plt.plot(*best_point, marker="*", ms=10,color="red")  # coordinates of the best point
+plt.plot(x, zeq(best_value, x), color='magenta', ls="--", lw=1.8, label=r"$\max\, Z = 3x_1 + 4x_2$")  # line plot for the best z function
+
+plt.legend(fontsize=12, loc="upper right")
+plt.xlim(-10,500)
+plt.ylim(-10,500)
+plt.show()
---
+++ 03_simplex/scripts/example03-03.py
@@ -0,0 +1,111 @@
+"""
+max Z = 2x1 + x2
+
+x1 + 2x2 <= 10
+x1 + x2 <= 6
+x1 - x2 <= 2
+x1 - 2x2 <= 1
+
+x1, x2 >= 0
+"""
+
+import numpy as np
+import  matplotlib.pyplot as plt
+
+
+
+# function domain
+x = np.linspace(0,100, 50)
+# ==== constraints =====
+eq1 = (10 - x)/2   # x2 = (10 - x1)/10
+eq2 = 6 - x        # x2 = 6 - x1
+eq3 = x - 2        # x2 = x1 - 2
+eq4 = (x - 1)/2    # x2 = (x1 - 1)/2
+# =======zfunction===========
+zeq = lambda x1, z: z - 2*x1   # x2 = Z - 2x1
+equations_list = [(x, eq1),
+                  (x, eq2),
+                  (x, eq3),
+                  (x, eq4)]
+
+equation_labels = [r"$x_1 + 2x_2 \leq 10$",  # blue
+                   r"$x_1 + x_2 \leq 6$",    # orange
+                   r"$x_1 - x_2 \leq 2$",    # green
+                   r"$x_1 - 2x_2 \leq 1$",   # red
+                   ]
+
+plt.figure(figsize=(8,8))
+plt.figure("equal")
+plt.axvline(0, color="0.4")
+plt.axhline(0, color="0.4")
+for equation, eqlabel in zip(equations_list, equation_labels):
+    plt.plot(*equation, lw=2, label=eqlabel)
+
+plt.plot(x, zeq(x, 0), color="magenta", lw=3, ls="--", label=r"$\max\, Z = 2x_1 + x_2$", alpha=0.4)
+for i in np.linspace(2, 6, 3):
+    plt.plot(x, zeq(x, i), color="magenta", lw=3, ls="--", alpha=0.4)
+plt.xlim(-1, 8)
+plt.ylim(-1, 8)
+plt.legend(fontsize=10, loc="upper right")
+plt.show()
+
+# coefficient matrix (left-hand side)
+A = np.array([ # Color   Index
+    [1,  2],   # blue    0
+    [1,  1],   # orange  1
+    [1, -1],   # green   2
+    [1, -2],   # red     3
+    [0,  1],   # gray    4 x Axis
+    [1,  0],   # gray    5 y Axis
+    ])
+
+# column-vector
+b = np.array([10,
+              6,
+              2,
+              1,
+              0, # x Axis
+              0, # y Axis
+              ])
+
+# Only lines with intersecting each other
+equations_index = [(4, 5),  # xAxis, yAxis 
+                   (4, 3),  # xAxis, red
+                   (2, 3),  # green, red
+                   (1, 2),  # orange, green
+                   (0, 1),  # blue, orange
+                   (0, 5),  # blue, yAxis
+                   ]
+
+points = [np.linalg.solve(A[list(i)], b[list(i)]) for i in equations_index]
+# print([*enumerate(2*x1 + x2 for x1, x2 in points)])
+best_index, best_value = max(enumerate(2*x1 + x2 for x1, x2 in points), key=lambda t:t[1])
+best_point = points[best_index]
+
+coordinates = [*zip(*points)]
+
+plt.figure(figsize=(8,8))
+plt.figure("equals")
+plt.axvline(0, color="0.4")
+plt.axhline(0, color="0.4")
+
+for eq,eqlabel in zip(equations_list, equation_labels):
+    plt.plot(*eq, lw=2, label=eqlabel)
+
+plt.plot(x, zeq(x, 0), color="magenta", lw=3, ls="--", alpha=0.4, label=r"$\max\, Z = 2x_1 + x_2$")
+for i in np.linspace(2, 6, 3):  # zvalue lines
+    plt.plot(x, zeq(x, i), color="magenta", lw=3, ls="--", alpha=0.4)
+plt.plot(x, zeq(x, best_value), color="magenta", lw=3, ls="--", alpha=0.6)  # optimal solution line
+
+for point in points:
+    plt.plot(*point, color="k", marker="o", ms=7)
+plt.plot(*best_point, color="red", marker="*", ms=14)
+
+plt.fill(*coordinates, facecolor="yellow", edgecolor="gray", lw=4, alpha=0.4)
+
+plt.xlim(-1, 8)
+plt.ylim(-1, 8)
+plt.legend(fontsize=10, loc="upper right")
+plt.show()
+  
+
