3103cefa5698e889284fd09bd7ea352a0e41a57f
rafaeltorrese <torresc.rafael@gmail.com>
2021-10-19 18:58:45 -0500

    Flaoat type in numpy arrays.

--- scripts/duality/dualsimplex_algorithm.py
+++ scripts/duality/dualsimplex_algorithm.py
@@ -1,6 +1,8 @@
 import numpy as np
+import pandas as pd
 
-def dual_simplex(matrix, rhs, z, numxvars):
+
+def dual_simplex(matrix, rhs, z, numxvars, varlabel='x'):
     '''Simplex algorithm to solve linear programming problems
 
     Parameters
@@ -17,13 +19,13 @@ def dual_simplex(matrix, rhs, z, numxvars):
     direction: {+1 , -1}
         For maximization problems use +1 and for minimization problems use -1 instead.
     '''
-    matrix = np.array(matrix)
-    rhs = np.array(rhs)
-    z = np.array(z)
+    matrix = np.array(matrix, dtype=float)
+    rhs = np.array(rhs, dtype=float)
+    z = np.array(z, dtype=float)
 
     num_rows, num_cols = matrix.shape
 
-    onecols = np.where(matrix == 1)[1]
+    onecols = np.where(matrix == 1 & (np.abs(matrix).sum(axis=0) == 1))[1]
     cb_index = onecols[onecols >= numxvars]
     cb = z[cb_index]
 
@@ -32,31 +34,34 @@ def dual_simplex(matrix, rhs, z, numxvars):
     net_evaluation = z - zj
 
     solutions = []
-    fvalues = []
+
+    labels = [f"{varlabel}{i + 1}" for i in range(num_cols)]
 
     iteration = 0
     if np.any(net_evaluation > 0):
-            print("Method Fails")
-            print("Net Evaluation Row", net_evaluation)
-    while np.all(net_evaluation <= 0) and np.any(rhs < 0) :
+        print("Method Fails")
+        print(f'Net Evaluation Row {net_evaluation}')
+        return
+    while np.all(net_evaluation <= 0) and np.any(rhs < 0):
         solution = np.zeros_like(z)
 
         leaving = rhs.argmin()   # leaving variables (index)
         key_row = matrix[leaving]
 
-        if np.all(key_row>=0):
+        if np.all(key_row >= 0):
             print("Infeasible Solution")
-            break
+            return
 
-        ratios = np.divide(net_evaluation, key_row, out=np.full_like(z, np.inf), where=key_row<0)
+        ratios = np.divide(net_evaluation, key_row,
+                           out=np.full_like(z, np.inf), where=key_row < 0)
 
         entering = ratios.argmin()
 
         pivot = matrix[leaving, entering]
 
         if pivot != 1:
-            matrix[leaving] = matrix[leaving] / pivot
-            rhs[leaving] = rhs[leaving] / pivot
+            matrix[leaving] = np.divide(matrix[leaving], pivot)
+            rhs[leaving] = np.divide(rhs[leaving], pivot)
 
         for i in range(num_rows):
             if i == leaving:
@@ -77,18 +82,31 @@ def dual_simplex(matrix, rhs, z, numxvars):
 
         print(f"Iteration {iteration}")
         print(matrix,  "\n")
-        print("Solution", solution, f"\tZ: {cb.dot(rhs):0.2f}", "\n")
+        print(f'Solution {solution} \t Z: {cb.dot(rhs):0.2f} \n')
 
         solutions.append(solution)
-        fvalues.append(cb.dot(rhs))
+
         if np.any(net_evaluation > 0):
             print("Method Fails")
-            break
+            return
 
         if np.all(net_evaluation <= 0) and np.all(rhs >= 0):
             print(f"Optimal solution found in {iteration} iterations")
 
-    return np.array(solutions), fvalues, np.vstack((zj, net_evaluation))
+    return pd.DataFrame(matrix, columns=labels), pd.DataFrame(np.vstack((zj, net_evaluation)), index=['zj', 'cj - zj'], columns=labels)
+
 
 if __name__ == '__main__':
-    pass
+
+    A = [
+        [-6, -1,  1,  0, 1,	0],
+        [-4, -2, -1, -1, 0,	1],
+    ]
+
+    b = [-5, -4]
+
+    Z = [-24, -6, -1, -2, 0, 0]
+
+    table, lastrows = dual_simplex(matrix=A, rhs=b, z=Z, numxvars=4)
+
+    print(table)
