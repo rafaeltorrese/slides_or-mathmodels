c81e52029b9fe251fb00b5aedbd1f9ffaf1d6c78
Rafael Torres Escobar <torresc.rafael@gmail.com>
2021-11-10 17:56:12 -0600

    Display results as fractions

--- scripts/sensitivity/exercise_01a.py
+++ scripts/sensitivity/exercise_01a.py
@@ -1,6 +1,7 @@
+from fractions import Fraction
 import numpy as np
 import pandas as pd
-
+np.set_printoptions(formatter={'all': lambda x: str(Fraction(x).limit_denominator())})
 
 from simplex_algorithm import simplex
 
@@ -17,13 +18,18 @@ zvector = np.array([3, 4, 1, 7, 0, 0, 0])
 sols, lastrows, table, bsolution, cbindx = simplex(matrix=A, rhs=b,
                                                        z=zvector,   direction=1)
 
-
-print(bsolution)
+print('Last Rows\n', lastrows, '\n')
+print(bsolution, '\n')
 dirname = r'C:\Users\rafael.torrese\Documents'
 
 # table.to_excel(f'{dirname}\ex01.xlsx')
 
 
-np.savetxt("ex01.csv", A, delimiter=",")
+# np.savetxt("ex01.csv", A, delimiter=",")
+
+base = A[:, cbindx]
+base_inv = np.linalg.inv(base)
+
+print('Inverse of Base Matrix\n', base_inv)
 
 
