24fd41cc8757f4c361a21acf8934120ee0113f06
Rafael Torres Escobar <torresc.rafael@gmail.com>
2022-04-06 15:03:14 -0500

    use tabulate package to improve readability. Use pandas library to return three dataframes: one for basis, one for whole matris, and one for only solution.

--- scripts/simplex_algorithm.py
+++ scripts/simplex_algorithm.py
@@ -1,8 +1,20 @@
+from tabulate import tabulate
+from fractions import Fraction
+
 import numpy as np
 import pandas as pd
 
+np.set_printoptions(
+    formatter={
+        'all': lambda x: str(Fraction(x).limit_denominator())
+    }
+)
 
-def simplex(matrix, rhs, z, numxvars, varlabel='x', direction=1):
+pd.set_option(
+    'display.float_format', lambda x: str(Fraction(x).limit_denominator())
+)
+
+def simplex(matrix, rhs, z, varlabel='x', direction=1):
     '''Simplex algorithm to solve linear programming problems
 
     Parameters
@@ -12,9 +24,6 @@ def simplex(matrix, rhs, z, numxvars, varlabel='x', direction=1):
 
     rhs: numpy ndarray
         Right-hand side vector
-
-    numxvars: int
-        Number of x variables
 
     direction: {+1 , -1}
         Use +1 for maximization and -1 for minimization.
@@ -31,78 +40,109 @@ def simplex(matrix, rhs, z, numxvars, varlabel='x', direction=1):
         Last rows of optimal table, this rows corresponde to Zj and cj - Zj
     '''
     print("="*10, "Simplex Method", "="*10)
-
-    matrix = np.array(matrix, dtype=float)
-    rhs = np.array(rhs, dtype=float)
-    z = np.array(z, dtype=float)
-
-    num_rows, num_cols = matrix.shape
-
-    onecols = np.where(matrix == 1 & (np.abs(matrix).sum(axis=0) == 1))[1]
-    cb_index = onecols[onecols >= numxvars]
+    matrix =np.array (matrix, dtype=float)
+    rhs= np.array (rhs, dtype=float)
+    z=np.array (z, dtype=float)
+    
+    num_rows, num_cols= matrix.shape
+    cb_index = np.nonzero((np.abs(matrix).sum(axis=0) == 1) & (matrix == 1))[1]
     cb = z[cb_index]
-
-    zj = cb.dot(matrix)
-
-    net_evaluation = direction * (z - zj)
-
-    solutions = []
-    fvalues = []
-
-    labels = [f"{varlabel}{i + 1}" for i in range(num_cols)]
-    basis = [labels[i] for i in cb_index]
-
+    
+    zj= cb.dot (matrix)
+    ner= direction*(z-zj) #ner:net evaluation rot
+    
+    labels = [f'{varlabel}{i}' for i in range (1, num_cols + 1)]    
+    basic_labels = [labels[idx] for idx in cb_index]
+    
+        
     iteration = 0
-    while np.any(net_evaluation > 0):
+    while np.any(ner > 0):
         solution = np.zeros_like(z)
-        entering = net_evaluation.argmax()  # entering variables (index)
-        entering_label = labels[entering]
-
-        key_col = matrix[:, entering]
-
-        ratios = np.divide(rhs, key_col,
-                           out=np.full_like(rhs, np.inf),
-                           where=key_col > 0)
-
-        leaving = ratios.argmin()   # leaving variables (index)
-        leaving_label = labels[cb_index[leaving]]
-        # ==================
-        pivot = matrix[leaving, entering]
-
-        if pivot != 1:
-            matrix[leaving] = matrix[leaving] / pivot
-            rhs[leaving] = rhs[leaving] / pivot
-
-        for i in range(num_rows):
+        entering= ner.argmax ()
+        entering_label=labels[entering]
+        
+        key_column= matrix[:, entering]
+        
+        ratios=np.full_like (rhs, np.inf)
+        np.divide (rhs, key_column, where=key_column>0, out=ratios)
+        
+        leaving= ratios.argmin()
+        leaving_label=labels[cb_index[leaving]]
+        
+        pivot=matrix[leaving, entering]
+        
+        if pivot !=1:
+            matrix [leaving]=matrix [leaving]/pivot
+            rhs [leaving]=rhs[leaving]/pivot
+            
+        #Gauss-Jordan
+        for i in range (num_rows):
             if i != leaving:
-                factor = matrix[i, entering]
-                matrix[i] = -factor * matrix[leaving] + matrix[i]
-                rhs[i] = -factor * rhs[leaving] + rhs[i]
-        # ==================
+                factor = matrix [i, entering]
+                matrix [i]=-factor * matrix [leaving] + matrix[i]
+                rhs [i]=-factor * rhs [leaving] + rhs [i]
+                
+        basic_labels[leaving] = entering_label
         cb_index[leaving] = entering
-
         cb = z[cb_index]
         zj = cb.dot(matrix)
-        net_evaluation = direction * (z - zj)
-
-        solution[cb_index] = rhs  # basics
-
-        iteration += 1
-        basis[leaving] = entering_label
-        print(f'Iteration {iteration}. {leaving_label} ---> {entering_label}')
-        print(matrix,  "\n")
-        print(f'Solution {solution} \t Z: {cb.dot(rhs):0.2f}')
-        print(f'cj - zj: {net_evaluation}')
-        print(f'rhs vector {direction * rhs}', "\n")
-
-        solutions.append(solution)
-        fvalues.append(cb.dot(rhs))
-        if np.all(net_evaluation <= 0):
-            print(f"Optimal solution found in {iteration} iterations")
-            # print((*zip(labels, np.round(solution, 3))))
-
-            print()
-            return pd.DataFrame(np.array(solutions), index=[f'iter{str(i).zfill(2)}' for i in range(1, iteration + 1)], columns=labels),  pd.DataFrame(np.vstack((zj, net_evaluation)), index=['zj', 'cj - zj'], columns=labels), pd.DataFrame(matrix, columns=labels, index=basis), pd.Series(rhs, index=basis), cb_index
+        ner = direction*(z-zj)
+        
+        iteration+=1
+        zvalue = cb.dot(rhs)
+        
+        print (f'Iteration {iteration}. \nIn:{entering_label}. Out:{leaving_label}')
+        
+        print(
+            tabulate(
+                np.hstack((matrix, rhs[:, np.newaxis])),
+                headers=labels+['b'],
+                showindex=basic_labels,
+#                 numalign='decimal',
+#                 disable_numparse=True,
+#                 floatfmt=Fraction().limit_denominator(),                
+#                 tablefmt='pipe',
+#                 tablefmt='pretty',
+                tablefmt='orgtbl',
+#                 tablefmt='github',
+                floatfmt='.3f',
+            ),
+            '\n',
+        )
+        print(
+            tabulate(
+                np.vstack((zj, direction * ner)),
+                showindex=['zj', 'cj-zj'],
+                headers=labels,
+                tablefmt='grid',
+            ),
+            '\n'
+        )
+                    
+        
+        if np.all (ner<=0):
+            print (f'Optimal solution found in {iteration} iteration(s)')
+            print(
+                tabulate(
+                    {
+                        'Basics': basic_labels, 
+                        'Values': rhs,
+                    },
+                    headers='keys',
+#                     tablefmt='github',
+#                     tablefmt='pipe',
+#                     tablefmt='tsv',
+                    tablefmt='orgtbl',
+#                     tablefmt='psql',
+#                     tablefmt='pretty',
+#                     floatfmt='.3f',
+                )
+             )
+            print(f'Zvalue: {zvalue.round(2)}')
+            
+    return pd.DataFrame({'index': cb_index, 'cb': cb}, index=basic_labels),\
+pd.DataFrame(np.vstack((matrix, zj, direction * ner)), columns=labels, index=basic_labels + ['zj', 'cj-zj']),\
+pd.Series(np.append(rhs, zvalue), index=basic_labels + ['z'], name='Solution')      
 
 
 if __name__ == '__main__':
@@ -115,11 +155,9 @@ if __name__ == '__main__':
 
     bprimal = [24, 6, 1, 2]
 
-    Zvector = [5, 4, 0, 0, 0, 0]
+    Zvector = [5, 4, 0, 0, 0, 0]    
 
-    nvars = 2
+    basis, main, sol = simplex(matrix=Aprimal, rhs=bprimal,
+                                                       z=Zvector,  direction=1)
 
-    sols, lastrows, table, bsolution, cbindx = simplex(matrix=Aprimal, rhs=bprimal,
-                                                       z=Zvector, numxvars=nvars,  direction=1)
-
-    print(sols.loc['iter02'])
+    print(basis)
