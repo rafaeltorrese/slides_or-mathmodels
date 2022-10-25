fc2520b156d5dadd78d6483bdc755c53b2dacf61
Rafael Torres Escobar <torresc.rafael@gmail.com>
2022-10-25 12:52:25 -0500

    add data test

--- scripts/sensitivity/example_01.py
+++ scripts/sensitivity/example_01.py
@@ -4,6 +4,8 @@ import pandas as pd
 
 from simplex_algorithm import simplex
 from dualsimplex_algorithm import dual_simplex
+
+from data import B
 # %%
 
 
@@ -27,30 +29,37 @@ if __name__ == '__main__':
         [2, -1, 3, 0, 1],
     ])
 
-    B = np.array([5, 2])
+    
     B1 = np.array([7, 2])
     B2 = np.array([3, 9])
 
-    nx = 3
+    
 
     solutions, lastrows, table, rhs, cbindx = simplex(
-        matrix=A, rhs=B, z=objfunc, numxvars=nx, direction=1)
+        matrix=A, rhs=B, obj_coef=objfunc, direction=1)
 
     print(solutions, '\n')
     print(table, '\n')
     print(lastrows, '\n')
     print(rhs, '\n')
 
+    optimal_table = pd.concat([table, rhs], axis=1)
+    print('\nOptimal Table')
+    print(optimal_table)
+
+    # optimal_table.to_excel(
+    #     r'C:\Users\rafael.torrese\Documents\anahuac\2022-03_agu-dec\operations-research\example.xlsx'
+    # )
+
     Basis = A[:, cbindx]
     Binv = np.linalg.inv(Basis)
-    print(Basis, '\n')
-    print(Binv, '\n')
+    print(f'\nBase Matrix \n{Basis}')
+    print(f'\nMatrix Inv Base \n{Binv}')
 
+    print()
     new_solution = check_feasebility(Basis, B2)
 
     # table.to_csv(r'F:\anahuac\Operations-Research\sensitivity\example01.csv')
 
-    optimal_table = pd.concat([table, rhs], axis=1)
-    print(optimal_table)
-    # optimal_table.to_excel(
-    #     r'F:\anahuac\Operations-Research\sensitivity\example_pandas01.xlsx')
+    
+    
